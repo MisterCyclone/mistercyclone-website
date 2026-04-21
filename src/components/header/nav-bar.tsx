d4fa77591a131ba254146ca09567268aabd28f45
MisterCyclone <cyclone@mistercyclone.co.uk>
2026-04-21 14:30:55 +0100

    Updated nav-bar to play sound.

--- src/components/header/nav-bar.tsx
+++ src/components/header/nav-bar.tsx
@@ -1,5 +1,8 @@
 import React from "react"
+import { useState } from "react";
 import { NavLink } from "react-router-dom"
+import useSound from "use-sound";
+import onNavButtonEnter from '../../assets/onNavButtonEnter.wav'
 
 interface NavBarProps {
   links : {
@@ -10,10 +13,22 @@ interface NavBarProps {
 
 const NavBar: React.FC<NavBarProps> = ({links}) => {
 
+  const [randomPitch, setRandomPitch] = useState<number>(1);
+
+  const [play] = useSound(onNavButtonEnter, {
+    playbackRate: randomPitch
+  });
+  
+  const handlePitchPlay = () => {
+    setRandomPitch(Number.parseFloat(((Math.random() * (1.1 - 0.9) + 0.9)).toFixed(1)));
+    console.log(randomPitch)
+    play();
+  }
+  
   return (
     <div className="nav-bar">
       {links.map((link) => (
-        <NavLink className="nav-bar-button" to={link.routeTo}>{link.label}</NavLink>
+        <NavLink className="nav-bar-button" onMouseEnter={handlePitchPlay} to={link.routeTo}>{link.label}</NavLink>
       ))}
     </div>
   )
