a79717e83ab5ea6b268d85f31d645dfcacddf4ed
MisterCyclone <cyclone@mistercyclone.co.uk>
2026-04-25 21:18:45 +0100

    Organised sound effects

---
+++ src/assets/sound-effects/sound-effects_index.ts
@@ -0,0 +1,17 @@
+import onButtonEnter from './onButtonEnter.wav'
+import onCardEnter from './onCardEnter.wav'
+import onClick from './onClick.wav'
+import onImageEnter from './onImageEnterwav.wav'
+import onNavButtonEnter from './onNavButtonEnter.wav'
+import onTagEnter from './onTagEnter.wav'
+
+const soundEffects = {
+  onButtonEnter,
+  onCardEnter,
+  onClick,
+  onImageEnter,
+  onNavButtonEnter,
+  onTagEnter,
+}
+
+export default soundEffects;
