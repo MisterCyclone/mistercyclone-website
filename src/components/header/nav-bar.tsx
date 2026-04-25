21a36d9c32e55c010a3f49746a465694aa82a2c8
MisterCyclone <cyclone@mistercyclone.co.uk>
2026-04-25 21:23:18 +0100

    Updated Styling to components and pages.

--- src/components/header/nav-bar.tsx
+++ src/components/header/nav-bar.tsx
@@ -1,10 +1,8 @@
 import React from "react"
 import './header.css'
-import { useState } from "react";
 import { NavLink } from "react-router-dom"
-import useSound from "use-sound";
-import onNavButtonEnter from '../../assets/onNavButtonEnter.wav'
-import onNavButtonClick from '../../assets/onNavButtonClicked.wav'
+import usePlaySoundPitched from "../../hooks/usePlaySoundPitched";
+import usePlaySound from "../../hooks/usePlaySound";
 
 interface NavBarProps {
   links : {
@@ -14,25 +12,13 @@ interface NavBarProps {
 }
 
 const NavBar: React.FC<NavBarProps> = ({links}) => {
-
-  const [randomPitch, setRandomPitch] = useState<number>(1);
-
-  const [play] = useSound(onNavButtonEnter, {
-    playbackRate: randomPitch
-  });
-
-  const [click] = useSound(onNavButtonClick);
-  
-  const handlePitchPlay = () => {
-    setRandomPitch(Number.parseFloat(((Math.random() * (1.1 - 0.9) + 0.9)).toFixed(1)));
-    console.log(randomPitch)
-    play();
-  }
+  const { playPitchByType } = usePlaySoundPitched();
+  const { playByType } = usePlaySound();
   
   return (
     <div className="nav-bar">
       {links.map((link) => (
-        <NavLink className="nav-bar-button" onClick={() => click()} onMouseEnter={handlePitchPlay} to={link.routeTo}>{link.label}</NavLink>
+        <NavLink className="nav-bar-button" key={link.routeTo} onClick={() => playByType('click')} onMouseEnter={() => playPitchByType('nav-button')} to={link.routeTo}>{link.label}</NavLink>
       ))}
     </div>
   )
