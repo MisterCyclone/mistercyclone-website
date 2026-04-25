dc1c7d3e12ae87f5183729aa0d371fcaa4923826
MisterCyclone <cyclone@mistercyclone.co.uk>
2026-04-25 21:34:46 +0100

    Updated home page to remove contact card

--- src/pages/homepage/homePage.tsx
+++ src/pages/homepage/homePage.tsx
@@ -4,21 +4,15 @@ import homeContent from '../../website-content/home.json'
 import LinkCard from "../../components/cards/linkCard";
 import './homePage.css'
 
-const aboutCard = homeContent["about-me"];
-const projectCard = homeContent.projects
-const experienceCard = homeContent.experience
-const contactCard = homeContent.contact;
-
 const HomePage: React.FC = () => {
   
   return (
     <div className="homepage-container">
       <h1 className="home-title">Welcome!</h1>
       <div className="home-container">
-        <HomeCard {...aboutCard}/>
-        <HomeCard {...projectCard}/>
-        <HomeCard {...experienceCard}/>
-        <HomeCard {...contactCard}/>
+        <HomeCard {...homeContent["about-me"]}/>
+        <HomeCard {...homeContent.projects}/>
+        <HomeCard {...homeContent.experience}/>
       </div>
       <h1 className="home-title">Contact Me!</h1>
       <div className="contact-link-container">
