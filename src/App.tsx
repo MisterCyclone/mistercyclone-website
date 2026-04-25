0635d146e655cb01f2e4fcfc4313976f2cfd3572
MisterCyclone <cyclone@mistercyclone.co.uk>
2026-04-25 21:25:49 +0100

    Updated global styling and added footer to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Header from './components/header/header'
+import Footer from './components/footer/footer'
 import HomePage from './pages/homepage/homePage'
 import AboutPage from './pages/aboutpage/aboutPage'
 import ProjectsPage from './pages/projectspage/projectsPage'
 import ExperiencePage from './pages/experiencepage/experiencePage'
-import ContactPage from './pages/contactpage/contactPage'
 import { Route, Routes } from 'react-router-dom'
+import footerContent from './website-content/footer.json'
 
 const App: React.FC = () => {
 
@@ -17,8 +18,8 @@ const App: React.FC = () => {
         <Route path="/about" Component={AboutPage}/>
         <Route path="/projects" Component={ProjectsPage}/>
         <Route path="/experience" Component={ExperiencePage}/>
-        <Route path="/contact" Component={ContactPage}/>
       </Routes>
+      <Footer links={[footerContent.github, footerContent.linkedin, footerContent.email]}/>
     </>
   )
 }
